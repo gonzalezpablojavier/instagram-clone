27685355c44ed20d59a7e8f926620c811e468709
pablo Gonzalez <pablo.j.gonzalez@gmail.com>
2024-07-18 14:34:53 -0300

    fondo login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -22,7 +22,9 @@ const Login: React.FC = () => {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+  
+      <div className="min-h-screen flex items-center justify-center bg-login-background bg-cover bg-center">
+
       <form 
         onSubmit={handleSubmit} 
         className="bg-white p-8 rounded shadow-md w-80 space-y-4 animate__animated animate__fadeIn"
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [ "./src/**/*.{js,jsx,ts,tsx}",],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {
+      backgroundImage: theme => ({
+        'login-background': "url(https://distrisuperapis.com.ar/bandera.jpg)",
+      }),
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
-}
-
+};
