7a91e2b908180f22819dab80dae7d4c7607c0d0c
pablo Gonzalez <pablo.j.gonzalez@gmail.com>
2024-07-21 16:05:08 -0300

    me paso a render

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'https://elated-kowalevski.51-222-158-198.plesk.page',
+  baseURL: 'https://rrhh-back.onrender.com',
   withCredentials: true, // Asegura que las credenciales se envíen con cada solicitud
   headers: {
     'Content-Type': 'application/json',
